06ddb5727eb2922adf331452113b3b77f2de526c
Børre Gaup <albbas@gmail.com>
2022-02-10 17:19:30 +0100

    Move app to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:satni_flutter/search/search.dart';
+import 'app.dart';
 
 void main() {
   runApp(
@@ -8,29 +8,4 @@ void main() {
       child: App(),
     ),
   );
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'sátni.org',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const SearchPage(),
-    );
-  }
 }
