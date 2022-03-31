b0a5edd288db2456883c8be3afba6642cf156983
Børre Gaup <albbas@gmail.com>
2022-03-31 17:40:08 +0200

    Show max three langs

--- lib/search/view/view.dart
+++ lib/search/view/view.dart
@@ -106,6 +106,21 @@ class Status extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final filter = ref.watch(filterProvider);
+    final availableDicts = [
+      {'name': 'termwiki'},
+      {'name': 'gtsmenob', 'src': 'sme', 'target': 'nob'},
+      {'name': 'gtnobsme', 'src': 'nob', 'target': 'sme'},
+      {'name': 'gtnobsma', 'src': 'nob', 'target': 'sma'},
+      {'name': 'gtsmanob', 'src': 'sma', 'target': 'nob'},
+      {'name': 'gtsmefin', 'src': 'sme', 'target': 'fin'},
+      {'name': 'gtfinsme', 'src': 'fin', 'target': 'sme'},
+      {'name': 'gtsmesmn', 'src': 'sme', 'target': 'smn'},
+      {'name': 'gtsmnsme', 'src': 'smn', 'target': 'sme'},
+      {'name': 'gtfinsmn', 'src': 'fin', 'target': 'smn'},
+      {'name': 'gtsmnfin', 'src': 'smn', 'target': 'fin'},
+      {'name': 'gtfinnob', 'src': 'fin', 'target': 'nob'},
+      {'name': 'sammallahtismefin', 'src': 'sme', 'target': 'fin'},
+    ];
     final srcLangs = [...filter.wantedSrcLangs];
     final targetLangs = [...filter.wantedTargetLangs];
 
@@ -120,7 +135,9 @@ class Status extends ConsumerWidget {
             ),
           ),
           child: Text(
-            srcLangs.join(','),
+            srcLangs.length < 4
+                ? srcLangs.join(',')
+                : '${srcLangs.length} langs',
           ),
         ),
         IconButton(
@@ -139,7 +156,9 @@ class Status extends ConsumerWidget {
             ),
           ),
           child: Text(
-            targetLangs.join(','),
+            targetLangs.length < 4
+                ? targetLangs.join(',')
+                : '${targetLangs.length} langs',
           ),
         ),
       ],
