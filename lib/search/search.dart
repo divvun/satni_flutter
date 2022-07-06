8273f4c70ef7893d4082c0175051e163c9dc32e9
Børre Gaup <albbas@gmail.com>
2022-07-06 10:16:00 +0200

    Give the debouncer file a better name

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -1,4 +1,4 @@
-export 'application/divvun_search_service.dart';
+export 'application/search_debounce_service.dart';
 export 'data/index.dart';
 export 'domain/index.dart';
 export 'presentation/index.dart';
