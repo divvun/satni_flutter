5c50c8d0add77bbaf3651c0cb7a2b107b46a4de4
Børre Gaup <albbas@gmail.com>
2022-07-08 23:00:14 +0200

    Hide debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -10,6 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'sátni.org',
       theme: ThemeData(
         primarySwatch: Colors.blue,
