85f83167cc626ec2fb52c93e7211246e827545f3
Børre Gaup <albbas@gmail.com>
2022-09-16 13:29:58 +0200

    Tests of graphql json to repo model conversion

--- packages/satni_client_repository/test/satni_client_repository_test.dart
+++ packages/satni_client_repository/test/satni_client_repository_test.dart
@@ -1,16 +1,127 @@
 import 'package:satni_client_repository/satni_client_repository.dart';
+import 'package:satni_client_repository/src/models/dict_entry.dart';
+import 'package:satni_client_repository/src/models/translation_group.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
+  group('Test conversion from the graphql backend to the repo models', () {
+    test('SpuriousAttribute', () {
+      expect(
+          SpuriousAttribute.fromJson({
+            "__typename": "SpuriousAttributeType",
+            "name": "nr",
+            "value": "Pl"
+          }),
+          SpuriousAttribute(name: 'nr', value: 'Pl'));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Lemma', () {
+      expect(
+          Lemma.fromJson({
+            "__typename": "LemmaType",
+            "lemma": "boahkut",
+            "presentationLemma": "boahkut",
+            "language": "sme",
+            "pos": "N",
+            "spuriousAttributes": [
+              {
+                "__typename": "SpuriousAttributeType",
+                "name": "nr",
+                "value": "Pl"
+              }
+            ]
+          }),
+          Lemma(
+              lemma: "boahkut",
+              presentationLemma: "boahkut",
+              pos: "N",
+              spuriousAttributes: [
+                SpuriousAttribute(name: "nr", value: "Pl")
+              ]));
     });
+  });
+
+  test('DictEntry', () {
+    expect(
+        DictEntry.fromJson({
+          "__typename": "DictEntryType",
+          "dictName": "gtsmenob",
+          "srcLang": "sme",
+          "targetLang": "nob",
+          "lookupLemmas": [
+            {
+              "__typename": "LemmaType",
+              "lemma": "boahkut",
+              "presentationLemma": "boahkut",
+              "language": "sme",
+              "pos": "N",
+              "spuriousAttributes": [
+                {
+                  "__typename": "SpuriousAttributeType",
+                  "name": "nr",
+                  "value": "Pl"
+                }
+              ]
+            }
+          ],
+          "translationGroups": [
+            {
+              "__typename": "TranslationGroupType",
+              "translationLemmas": [
+                {
+                  "__typename": "LemmaType",
+                  "lemma": "vaksine",
+                  "presentationLemma": "vaksine",
+                  "language": "nob",
+                  "pos": "N"
+                }
+              ],
+              "exampleGroups": [
+                {
+                  "__typename": "ExampleGroupType",
+                  "example":
+                      'Das leat dieđut boahkuheami birra ja makkár oalgeváikkuhusat sáhttet leat boahkuin.',
+                  "translation":
+                      "Der er det informasjon om vaksineringa og hvilke bivirkninger som kan være av vaksinen."
+                }
+              ]
+            }
+          ]
+        }),
+        DictEntry(
+            dictName: 'gtsmenob',
+            srcLang: 'sme',
+            targetLang: 'nob',
+            lookupLemmas: [
+              Lemma.fromJson({
+                "lemma": "boahkut",
+                "presentationLemma": "boahkut",
+                "language": "sme",
+                "pos": "N",
+                "spuriousAttributes": [
+                  {"name": "nr", "value": "Pl"}
+                ]
+              })
+            ],
+            translationGroups: [
+              TranslationGroup.fromJson({
+                "translationLemmas": [
+                  {
+                    "lemma": "vaksine",
+                    "presentationLemma": "vaksine",
+                    "language": "nob",
+                    "pos": "N"
+                  }
+                ],
+                "exampleGroups": [
+                  {
+                    "example":
+                        'Das leat dieđut boahkuheami birra ja makkár oalgeváikkuhusat sáhttet leat boahkuin.',
+                    "translation":
+                        "Der er det informasjon om vaksineringa og hvilke bivirkninger som kan være av vaksinen."
+                  }
+                ]
+              })
+            ]));
   });
 }
