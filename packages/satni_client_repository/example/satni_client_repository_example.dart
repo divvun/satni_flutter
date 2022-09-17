5171db5e02ab473b2a1725c697f2d63d06faa883
Børre Gaup <albbas@gmail.com>
2022-09-17 23:26:10 +0200

    Comment to avoid lint warnings

--- packages/satni_client_repository/example/satni_client_repository_example.dart
+++ packages/satni_client_repository/example/satni_client_repository_example.dart
@@ -1,6 +1,6 @@
-import 'package:satni_client_repository/satni_client_repository.dart';
+// import 'package:satni_client_repository/satni_client_repository.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // var awesome = Awesome();
+  // print('awesome: ${awesome.isAwesome}');
 }
