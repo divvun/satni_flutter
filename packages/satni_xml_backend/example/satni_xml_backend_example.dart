e23b4eab79f78db37a0909ca7b73fc1a7dc57f3e
Børre Gaup <albbas@gmail.com>
2022-09-20 10:25:13 +0200

    Quelch warnings

--- packages/satni_xml_backend/example/satni_xml_backend_example.dart
+++ packages/satni_xml_backend/example/satni_xml_backend_example.dart
@@ -1,6 +1,6 @@
-import 'package:satni_xml_backend/satni_xml_backend.dart';
+// import 'package:satni_xml_backend/satni_xml_backend.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // var awesome = Awesome();
+  // print('awesome: ${awesome.isAwesome}');
 }
