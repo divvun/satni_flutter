2dd4972ba20189bda763a33fae0dd80f2275c28b
Børre Gaup <albbas@gmail.com>
2022-09-30 14:52:31 +0200

    Use local backend for testing

--- lib/src/common_providers/satni_service_provider.dart
+++ lib/src/common_providers/satni_service_provider.dart
@@ -3,4 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:satni_graphql_service/satni_graphql_service.dart';
 
 final satniServiceProvider = Provider(
-    (ref) => SatniGraphQLService(uri: 'https://satni.uit.no/newsatni/'));
+  // (ref) => SatniGraphQLService(uri: 'https://satni.uit.no/newsatni/'),
+  (ref) => SatniGraphQLService(uri: 'http://localhost:8000/graphql/'),
+);
